eb357e8a3f588d4932681eece282ef8eb09a91d9
KovalovIE <kovalevie1@gmail.com>
2018-01-22 14:23:00 +0200

    first commit

---
+++ gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+        //Less to css
+      less: {
+          development: {
+              options: {
+                  paths: ['src/styles']
+              },
+              files: {
+                  'dist/css/style.css': 'src/styles/style.less'
+              }
+          }
+      }
+  });
+
+    //Load Tasks
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    //Register Tasks
+    grunt.registerTask('default', ['less']);
+};
